be7323f602c5ae680a67293532705e978d4d0845
sondeko143 <sondeko143@gmail.com>
2023-05-31 01:06:37 +0900

    relax omegaconf version

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ def do_setup(package_data):
             "cffi",
             "cython",
             "hydra-core>=1.3.0",
-            "omegaconf<2.1",
+            "omegaconf",
             "numpy>=1.21.3",
             "regex",
             "sacrebleu>=1.4.12",
